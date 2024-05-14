91386a162b406d8726b6c2e3f4ef9cd10e85375a
Jacob <admin@auroraweb.com.au>
2024-05-15 09:12:45 +1000

    Update Laravel installation guide for groupwork

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
-    <p>&copy; AuroraWeb.com.au &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
+    <p>&copy; <a href="https://www.auroraweb.com.au/">AuroraWeb.com.au</a> &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
         &middot; <a href="https://github.com/JacobYZ/Web-Development-Conference">GitHub</a>
     </p>
-
 </footer>
