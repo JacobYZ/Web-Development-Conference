be51cdf83e61db837e3cf6222bb24f5af3576b0a
Jacob <admin@auroraweb.com.au>
2024-05-21 15:44:50 +1000

    refactor: Add 'password' field to User model migration and remove nullable constraint
    - In the database migration file `2014_10_12_000000_create_users_table.php`, added the 'password' field to the 'users' table.
    - Removed the nullable constraint for the 'password' field, making it required for all users.
    - This change ensures that all users have a non-null password for authentication purposes.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('affiliate', 100);
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
